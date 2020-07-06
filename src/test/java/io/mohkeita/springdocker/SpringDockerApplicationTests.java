d8381d4f81da4350ffc777d72d189412d851800c
mohkeita <mohkeita13@gmail.com>
2020-07-06 23:02:24 +0200

    Initial commit

---
+++ src/test/java/io/mohkeita/springdocker/SpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
